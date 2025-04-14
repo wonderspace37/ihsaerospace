56c0b684ff8ff07e0bbfe32c42597a7af773eb34
wonderspace37 <136860951+wonderspace37@users.noreply.github.com>
2025-04-13 22:27:41 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log('Hello!');
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    toggleButton.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
